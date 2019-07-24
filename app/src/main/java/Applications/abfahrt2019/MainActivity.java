76fbdd4fcac45daf37524cd705a2df5d2d29fb18
Lukas Hempel <lh11jise@studserv.uni-leipzig.de>
2019-07-24 11:03:14 +0200

    Fragen richtig eingelesen

--- app/src/main/java/Applications/abfahrt2019/MainActivity.java
+++ app/src/main/java/Applications/abfahrt2019/MainActivity.java
@@ -4,49 +4,263 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
-
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
+    // Attributes
+    private HashSet<Slice> fragenNormal;
+    private HashSet<Slice> spieleNormal;
+    private HashSet<Slice> virusNormal;
+    private HashSet<Slice> fragenWarm;
+    private HashSet<Slice> spieleWarm;
+    private HashSet<Slice> virusWarm;
+    private HashSet<Slice> fragenHeiss;
+    private HashSet<Slice> spieleHeiss;
+    private HashSet<Slice> virusHeiss;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private HashSet<String> spieler;
 
+    private int sliceCount;
+    private int anzahlSlices;
 
-        Button startButton = findViewById(R.id.startButton);
+    private String[] fragenNormalArray =  {
+            "Alle, die schon einmal an einem öffentlichen Ort Sex hatten, trinken 3 Schlucke.",
+            "Die letzte Person, die ihre Füße vom Bodem hebt trinkt 2 Schlucke.",
+            "X leckt die Nase von Y oder trinkt 2 Schlucke!"
+    };
+    private String[] spieleNormalArray =  {
+            "X und Y spielen Piss-Pott(gegenüberstehend abwechselnd ein Fuß vor dem anderen stellen, bis ihr euch in der Mitte trefft). Der/Die Verlierer/in trinkt 4 Schlucke.",
+            "Lieber Ausschweifungen mit einer Sexnummer haben oder FKK mit Freunden/Freundinnen machen? Stimmt alle gleichzeitig ab, die Verlierer trinken 2 Schlucke."
+    };
+    private String[] virusNormalArray =  {
+            "X und Y sind das neue Dreamteam, wenn X trinkt, trinkt Y mit. > X und Y, ihr seid doch kein Dreamteam und dürft wieder alleine trinken."
+    };
+    private String[] fragenWarmArray = {
+            "Alle trinken und ziehen ein Kleidungsstück aus!",
+            "X zieht Y ein Kleidungsstück aus oder trinkt 4 Schlucke."
+    };
+    private String[] spieleWarmArray =  {
+            "Boxershorts-Contest! Die Jungs zeigen alle ihre Boxershorts. Die Mädels bilden die Jury und wählen die schönste. Der Sieger verteilt 5 Schlücke.",
+            "String-Contest! Die Mädels zeigen alle ihren String. Die Jungs bilden die Jury und wählen den schönsten. Die Siegerin verteilt 5 Schlücke."
+    };
+    private String[] virusWarmArray =  {
+            "Loch-Spiel! Wann immer ihr wollt, formt einen Kreis mit Daumen und Zeigefinger zwischen Hüfte und Schulter. Jeder der reinguckt, zieht ein Kleidungsstück aus oder trink 4 Schlucke. > Das Loch-Spiel ist Vorbei!"
+    };
+    private String[] fragenHeissArray = {
+            "X, wähle eine Person, die die Hand an deine Unterwäsche legen soll. 4 Schlucke, wenn er/sie sich weigert.",
+            "X gibt Y einen Kuss auf den nackten Hintern oder trinkt 4 Schlucke."
+    };
+    private String[] spieleHeissArray =  {
+            "Nacheinander wählt jeder eine Person, die ein Kleidungsstück ausziehen muss. Weigert sie sich, trinkt sie 2 Schlucke. X beginnt.",
+            "Wettbewerb des schönsten Hinterns: Die Kerle machen die Show, die Mädels bilden die Jury. Der Gewinner verteilt 5 Schlucke."
+    };
+    private String[] virusHeissArray =  {
 
-        /*
-        startButton.setOnClickListener(new View.OnClickListener() {
+    };
 
-            @Override
-            public void onClick(View v) {
-
-            }
-
-        });
-        */
-        startButton.setOnClickListener(view ->  {
-            Log.d("debug", "Button clicked");
-            Abfahren();
-            // Hier kommt der Aufruf unserer Anfangsmethode rein
-        });
-    }
 
     private void Abfahren() {
         //
         // Bereinige zuerst Oberfläcche
 
 
-        // Dann hol alle Slices zusammen in For-Schleifen und fülle HashSets
+        // Initialisiere Sets usw
+        Initialize();
+        // Theoretisch müssten jetzt alle Fragen im entsprechenden Set sein. Jetzt setzen wir die Namen in die Fragen ein.
+        Interact();
+
+
+
+
+
+        // Debug:
+
+        if(fragenNormal.isEmpty()) Log.d("debug", "Empty");
+        // DebugTime, grab a Slice and represent it
+        for(Slice s1 : spieleHeiss) {
+            Log.d("debug", s1.toString());
+        }
+
+
+
+
+
+
+
+        // Dann hol alle Slices zusammen in For-Schleifen und fülle HashSets, beim UPsetten müssen die Fragenb mit dem X bearbeitet werden. (Interaktiv -> Boolean + Random Wahl)
+    }
+
+    // UI
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Linke den Button zu unserer Start-Methode
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(view ->  {
+            Log.d("debug", "Button clicked");
+            Abfahren();
+            Log.d("debug", "Abgefahren");
+            // Hier kommt der Aufruf unserer Anfangsmethode rein
+        });
+    }
+
+    private void Interact() {
+        // Eliminiere alle X, Y, Z usw; Wähle zufällige Zahl zw. 0 und Anzahl Spieler, dieser Spieler kommt an Stelle X. Achtung, Keine DOPPLUNG!
+    }
+
+    private void Initialize() {
+        fragenNormal = new HashSet<Slice>();
+        spieleNormal = new HashSet<Slice>();
+        virusNormal = new HashSet<Slice>();
+        fragenWarm  = new HashSet<Slice>();
+        spieleWarm = new HashSet<Slice>();
+        virusWarm = new HashSet<Slice>();
+        fragenHeiss = new HashSet<Slice>();
+        spieleHeiss = new HashSet<Slice>();
+        virusHeiss = new HashSet<Slice>();
+        spieler = new HashSet<String>();
+
+        sliceCount = 0;
+
+
+        // Fragen
+        // Normale Fragen
+        for(int i = 0; i < fragenNormalArray.length ; i++) {
+            String current = fragenNormalArray[i];
+
+            // InteraktivitätsCheck
+            boolean temp = false;
+            if(current.contains("X")) {
+                temp = true;
+            }
+
+            Slice slice = new Slice(current, Slice.Level.Normal, temp);
+            // Rein in den FragenPool
+            fragenNormal.add(slice);
+        }
+
+        // Warme Fragen
+        for(int i = 0; i < fragenWarmArray.length ; i++) {
+            String current = fragenWarmArray[i];
+
+            // InteraktivitätsCheck
+            boolean temp = false;
+            if(current.contains("X")) {
+                temp = true;
+            }
+
+            Slice slice = new Slice(current, Slice.Level.Warm, temp);
+            // Rein in den FragenPool
+            fragenWarm.add(slice);
+        }
+
+        // Heisse Fragen
+        for(int i = 0; i < fragenHeissArray.length ; i++) {
+            String current = fragenHeissArray[i];
+
+            // InteraktivitätsCheck
+            boolean temp = false;
+            if(current.contains("X")) {
+                temp = true;
+            }
+
+            Slice slice = new Slice(current, Slice.Level.Heiss, temp);
+            // Rein in den FragenPool
+            fragenHeiss.add(slice);
+        }
+
+
+        // Spiele
+        // Normale Spiele
+        for(int i = 0; i < spieleNormalArray.length ; i++) {
+            String current = spieleNormalArray[i];
+
+            // InteraktivitätsCheck
+            boolean temp = false;
+            if(current.contains("X")) {
+                temp = true;
+            }
+
+            Slice slice = new Slice(current, Slice.Level.Normal, temp);
+            // Rein in den FragenPool
+            spieleNormal.add(slice);
+        }
+        // Warme Spiele
+        for(int i = 0; i < spieleWarmArray.length ; i++) {
+            String current = spieleWarmArray[i];
+
+            // InteraktivitätsCheck
+            boolean temp = false;
+            if(current.contains("X")) {
+                temp = true;
+            }
+
+            Slice slice = new Slice(current, Slice.Level.Warm, temp);
+            // Rein in den FragenPool
+            spieleWarm.add(slice);
+        }
+
+        // Heisse Spiele
+        for(int i = 0; i < spieleHeissArray.length ; i++) {
+            String current = spieleHeissArray[i];
+
+            // InteraktivitätsCheck
+            boolean temp = false;
+            if(current.contains("X")) {
+                temp = true;
+            }
+
+            Slice slice = new Slice(current, Slice.Level.Heiss, temp);
+            // Rein in den FragenPool
+            spieleHeiss.add(slice);
+        }
+
+        // Viren
+        // Normale Viren
+        for(int i = 0; i < virusNormalArray.length ; i++) {
+            String current = virusNormalArray[i];
+
+            // InteraktivitätsCheck
+            boolean temp = false;
+            if(current.contains("X")) {
+                temp = true;
+            }
+
+            Slice slice = new Slice(current, Slice.Level.Normal, temp);
+            // Rein in den FragenPool
+            virusNormal.add(slice);
+        }
+        // Warme Viren
+        for(int i = 0; i < virusWarmArray.length ; i++) {
+            String current = virusWarmArray[i];
+
+            // InteraktivitätsCheck
+            boolean temp = false;
+            if(current.contains("X")) {
+                temp = true;
+            }
+
+            Slice slice = new Slice(current, Slice.Level.Warm, temp);
+            // Rein in den FragenPool
+            virusWarm.add(slice);
+        }
+
+        // Heisse Viren
+        for(int i = 0; i < virusHeissArray.length ; i++) {
+            String current = virusHeissArray[i];
+
+            // InteraktivitätsCheck
+            boolean temp = false;
+            if(current.contains("X")) {
+                temp = true;
+            }
+
+            Slice slice = new Slice(current, Slice.Level.Heiss, temp);
+            // Rein in den FragenPool
+            virusHeiss.add(slice);
+        }
     }
 }
-
-
-
-
-
-
-
-
 // Zuerst die Slices initialisieren, danach die HashSets füllen und den FragenCounter anlegen. Fehlt noch komplett die Spieler Auswahl usw
--- app/src/main/java/Applications/abfahrt2019/Slice.java
+++ app/src/main/java/Applications/abfahrt2019/Slice.java
@@ -4,12 +4,27 @@ import android.graphics.Color;
 import android.graphics.fonts.Font;
 
 public class Slice {
+    public enum Level {
+        Normal,
+        Warm,
+        Heiss
+    }
+
+    private Level kategorie;
     private String beschreibung;
- //   private enum kategorie;
     private Color farbe;
     private Font style;
+    private boolean interaktiv;
 
-    public Slice(String beschreibung) {
+
+    public Slice(String beschreibung, Level Kategorie, boolean interaktiv) {
         this.beschreibung = beschreibung;
+        this.interaktiv = interaktiv;
+        this.kategorie = Kategorie;
+    }
+
+    public String toString() {
+        String s = "Text: " + this.beschreibung + "; Kategorie: " + this.kategorie + ", Interaktiv: " + this.interaktiv + ", Farbe: " + this.farbe + ", Style: " + this.style;
+        return s;
     }
 }
